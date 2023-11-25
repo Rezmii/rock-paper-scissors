4ccc6fb7e98ef24610329d6a5f7148dfa46d2c78
Rezmii <bartoszrezmer20@gmail.com>
2023-11-25 13:21:02 +0100

    Adding getComputerChoice function to randomly get computer choice

---
+++ script.js
@@ -0,0 +1,8 @@
+function getComputerChoice() {
+  let rng = Math.floor(Math.random() * 3);
+  let computerChoice = "";
+  if (rng === 0) computerChoice = "Rock";
+  else if (rng === 1) computerChoice = "Paper";
+  else computerChoice = "Scissors";
+  return computerChoice;
+}
